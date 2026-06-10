66c06db4a502ffc9f85647edb9c799ff24eb96c5
Parag <paragshrivastavadatia@gmail.com>
2026-06-10 16:01:13 +0530

    changed the bot.py code for railway deployment

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+
+'''
+
 import os
+import json
 from dotenv import load_dotenv
 import gspread
 from google.oauth2.service_account import Credentials
@@ -18,8 +22,17 @@ SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets"
 ]
 
-creds = Credentials.from_service_account_file(
-    "expense-tracker-499004-283d23e44dbd.json",
+# creds = Credentials.from_service_account_file(
+#     "expense-tracker-499004-283d23e44dbd.json",
+#     scopes=SCOPES
+# )
+
+google_creds = json.loads(
+    os.getenv("GOOGLE_CREDENTIALS")
+)
+
+creds = Credentials.from_service_account_info(
+    google_creds,
     scopes=SCOPES
 )
 
@@ -67,6 +80,98 @@ async def expense(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # MAIN
 # =====================
 print("BOT TOKEN:", BOT_TOKEN)
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("expense", expense))
+
+print("Bot Started Successfully")
+
+app.run_polling()
+'''
+
+
+import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+from datetime import datetime
+
+# =====================
+# CONFIG
+# =====================
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets"
+]
+
+google_creds = json.loads(
+    os.getenv("GOOGLE_CREDENTIALS")
+)
+
+creds = Credentials.from_service_account_info(
+    google_creds,
+    scopes=SCOPES
+)
+
+client = gspread.authorize(creds)
+
+sheet = client.open_by_key(
+    "1szlGWdA8GWq9jkzAm3ZGxFbbuaGNbXeiIFjQoA26fyM"
+).worksheet("Sheet1")
+
+# =====================
+# COMMANDS
+# =====================
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Expense Tracker Ready!\n\n"
+        "Example:\n"
+        "/expense 120 Food Lunch"
+    )
+
+async def expense(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    try:
+        if len(context.args) < 3:
+            await update.message.reply_text(
+                "Usage:\n/expense 120 Food Lunch"
+            )
+            return
+
+        amount = context.args[0]
+        category = context.args[1]
+        description = " ".join(context.args[2:])
+
+        sheet.append_row([
+            datetime.now().strftime("%Y-%m-%d %H:%M"),
+            amount,
+            category,
+            description
+        ])
+
+        await update.message.reply_text(
+            f"✅ Added ₹{amount} for {category}"
+        )
+
+    except Exception as e:
+        print("ERROR:", str(e))
+
+        await update.message.reply_text(
+            "Usage:\n/expense 120 Food Lunch"
+        )
+
+# =====================
+# MAIN
+# =====================
+
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN not found")
 
 app = Application.builder().token(BOT_TOKEN).build()
 
