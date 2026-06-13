999c46ec553a7a3fdf7dca4f365cc7c8fa5b931b
Parag <paragshrivastavadatia@gmail.com>
2026-06-13 11:25:07 +0530

    Fix conflict error on restart

--- bot.py
+++ bot.py
@@ -171,4 +171,4 @@ app.add_handler(CommandHandler("expense", expense))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 print("Bot Started Successfully")
-app.run_polling()
+app.run_polling(drop_pending_updates=True)
